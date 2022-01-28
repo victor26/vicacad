f32220951de937a16f7d8fd0aeeac82c260b5f69
victor26 <victor-sousa1996@hotmail.com>
2022-01-28 18:21:23 -0300

    Update loginController.php
    teste

--- CRUD2/Controlador/loginController.php
+++ CRUD2/Controlador/loginController.php
@@ -21,20 +21,16 @@ $login = $loginDAO->login($login, $senha);
 
 
 if(!empty($login)){
-    $_SESSION["login"] = $login["login"];
-    $_SESSION["perfil"] = $login["perfil"];
-   switch ($_SESSION["perfil"]) {
-        case "Funcionario":
             echo "<script>";
             echo "   window.location='../../indexFuncionario.php'";
             echo "</script>";
             break;
-        case "Cliente":
+        /*case "Cliente":
             echo "<script>";
             echo "   window.location='../../indexCliente.php'";
             echo "</script>";
-            break;
-        }
+            break;*/
+        
     } else {
     echo "<script>";
     echo "   window.location='../../index.php'";
